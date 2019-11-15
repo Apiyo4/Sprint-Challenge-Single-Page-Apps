903bae0cd2053c8c6d3c95c501bb4b787416fc65
Apiyo4 <odingoannette@gmail.com>
2019-11-15 12:09:42 +0300

    Display data from Api

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacters] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
 
+    const getCharacters = ()=>{
+      axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response =>{
+        const dataArr = Object.values(response.data);
+        console.log(dataArr[1]);
+        setCharacters(dataArr[1])
+      })
+      .catch(error=>{
+        console.log(error)
+      })
+    }
+    getCharacters();
+  }, []);
+    if(!characters || characters.length === 0){
+      return <h3>Loading...</h3>
+    }
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        characters.map(character =>{
+          return(
+         <div key={character.id}>
+          <img  src={character.image} alt={`${character.name}  image`}/>
+              <p>{character.name}</p>
+            </div>
+            );
+        })
+      }
     </section>
   );
 }
